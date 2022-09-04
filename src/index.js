1e50d6cf1712574ba1d5d5d7460992467465d1ad
sourav mete <97776357+souravmete@users.noreply.github.com>
2022-09-04 12:15:07 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Heading";
+
+ReactDOM.render(
+  <div>
+    <Heading></Heading>
+    <p>This is a React js bootcamp taken up by Shaurya Sinha</p>
+  </div>,
+  document.getElementById("root")
+);
